c57ffbf38522c72aa8b10272feeb88813270dc35
Walker Brownfield <walkerbrownfield@gmail.com>
2018-10-05 23:20:08 -0500

    Notmin Follow
    Notmin follow character

--- Pikmin Rogue-like/Assets/NotminMovement.cs
+++ Pikmin Rogue-like/Assets/NotminMovement.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Notmin : MonoBehaviour {
+public class NotminMovement : MonoBehaviour 
+{
+
+    private GameObject wayPoint;
+    private Vector3 wayPointPos;
+
+    //Notmin Movement speed.
+    public float speed;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        //At start, Notmin find the gameobject called wayPoint
+        wayPoint = GameObject.Find("wayPoint");
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        wayPointPos = new Vector3(wayPoint.transform.position.x, wayPoint.transform.position.y, wayPoint.transform.position.z);
+        //Here, Notmin follow waypoint.
+        transform.position = Vector3.MoveTowards(transform.position, wayPointPos, speed * Time.deltaTime);
+    }
 }
---
+++ Pikmin Rogue-like/Assets/PlayerWaypoint.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWaypoint : MonoBehaviour {
+
+    //In editor, add your wayPoint gameobject to the script
+    public GameObject wayPoint;
+    //How often your waypoint's position will update to the player's position
+    private float timer = 0.5f;
+
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		if(timer>0)
+        {
+            timer -= Time.deltaTime;
+        }
+        if(timer <= 0)
+        {
+            //Position update
+            UpdatePosition();
+            timer = 0.5f;
+        }
+	}
+
+    void UpdatePosition()
+    {
+        //the wayPoint's position will now be the player's current position.
+        wayPoint.transform.position = transform.position;
+    }
+}
