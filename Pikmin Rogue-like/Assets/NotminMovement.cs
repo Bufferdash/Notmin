da6d6acc3c4aa6210a64173a0c8467f4e224622a
Walker Brownfield <walkerbrownfield@gmail.com>
2018-10-10 15:55:12 -0500

    Tried adding a follow radius
    Changes in NotminMovement defining Dist so they check to see if they are so far away before following. Game freezes on startup

--- Pikmin Rogue-like/Assets/NotminMovement.cs
+++ Pikmin Rogue-like/Assets/NotminMovement.cs
@@ -7,7 +7,8 @@ public class NotminMovement : MonoBehaviour
 
     private GameObject wayPoint;
     private Vector3 wayPointPos;
-    
+
+    public float lookAtDistance;
     //Notmin Movement speed.
     public float speed;
 
@@ -21,7 +22,8 @@ public class NotminMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        //if (Distance > lookAtDistance)
+        float Dist = Vector3.Distance(wayPoint.transform.position, transform.position);
+        while (Dist > lookAtDistance)
         {
             wayPointPos = new Vector3(wayPoint.transform.position.x, wayPoint.transform.position.y, wayPoint.transform.position.z);
             //Here, Notmin follow waypoint.
