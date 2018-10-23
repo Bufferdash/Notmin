d53e67c53a87256e1bc51dceda5d12fb1b491cf6
Walker Brownfield <walkerbrownfield@gmail.com>
2018-10-23 18:51:39 -0500

    Enemy Roam

---
+++ Pikmin Rogue-like/Assets/Scripts/EnemyRoam.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRoam : MonoBehaviour
+{
+    /*
+    //Example 1
+    Vector3 startPosition;
+    public int roamRadius;
+    */
+
+
+    // Use this for initialization
+    void Start()
+    {
+        /*
+        //Example 1
+        startPosition = transform.position;
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        // Example 1
+        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * roamRadius;
+        randomDirection += startPosition;
+        UnityEngine.AI.NavMeshHit hit;
+        UnityEngine.AI.NavMesh.SamplePosition(randomDirection, out hit, roamRadius, 1);
+        Vector3 finalPosition = hit.position;
+        _nav.destination = finalPosition;
+        */
+    }
+}
--- Pikmin Rogue-like/Assets/Scripts/PlayerMovement.cs
+++ Pikmin Rogue-like/Assets/Scripts/PlayerMovement.cs
@@ -27,15 +27,17 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
     {
+        /*
         //MOVEMENT
         var x = Input.GetAxis("Horizontal"); //* Time.deltaTime * speed;
         var z = Input.GetAxis("Vertical"); //* Time.deltaTime * speed;
         Vector3 input = new Vector3(x, 0, z);
         input.Normalize();
         transform.Translate(input * Time.deltaTime * speed);
+        */
         //transform.Translate(x, 0, 0);
         //transform.Translate(0, 0, z);
-        /*
+        ///*
         //Move Forward
         if(Input.GetKey(KeyCode.W))
         {
@@ -55,7 +57,7 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetKey(KeyCode.A))
         {
             transform.Translate(-Vector3.right * speed * Time.deltaTime);
-        }*/
+        }//*/
     }
 
     //Update is called once per frame
